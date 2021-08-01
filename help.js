11e8726c7d8370039fc820f7f25e4c2adc3d7356
madloyer <87596107+madloyer@users.noreply.github.com>
2021-08-01 12:43:16 +0700

    Update help.js

--- help.js
+++ help.js
@@ -8,6 +8,8 @@ module.exports.botcommand = `<u>Berikut adalah beberapa perintah dan penggunaan 
 \n\nKalau gak mau beli atau mau gratisan bisa juga langsung bikin sendiri dengan gunain APK 2NDLINE\nBisa download di Playstore ataupun Googling aja dan untuk Tutorial bisa cek YT admin dibawah:
 \n<a href="https://youtu.be/920nA1E8ON8">Tutorial Bikin Nomor US (2ndline)</a>
 \nIkuti tutorialnya yang benar biar bisa langsung dapet dan jangan nyerah jika gagal #tetapcoliwalaugakberguna
+\n\nUntuk Contoh testi atau contoh group yang ke banned nomor indo sehingga butuh akses Nomor Us bisa kalian kepo di group dibawah ini:
+\n<a href="https://t.me/testimonijasanous/28">Testimoni Mantapjozz</a>
 `;
 
 module.exports.botinstall = `<u>List Join Group Vip PPV / Full Genre Indo, asian, West.</u>
